4b93779f11ebbd00df89c04af56474e68f88b144
Antonioisrooted <antoniommaina@gmail.com>
2020-04-20 05:25:36 +0300

    creating loops

--- src/main/java/Caesar.java
+++ src/main/java/Caesar.java
@@ -7,6 +7,23 @@ public class Caesar  {
         else if(shift<0){
             shift =(shift%26);
         }
+        String cipherText = "";
+        int length = plainText.length();
+        for(int i = 0; i<length; i++){
+            char ch = plainText.charAt(i);
+            if(Character.isLetter(ch)){
+                if(Character.isLowerCase(ch)){
+
+                }
+                else if(Character.isUpperCase(ch)){
+
+                }
+            }
+            else{
+                cipherText += ch;
+            }
+        }
+        return cipherText;
     }
 
     public static void main(String[] args){
