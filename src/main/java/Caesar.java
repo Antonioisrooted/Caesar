5cd26479bdefa403235d9d5e2ac2a8867b0cfac1
Antonioisrooted <antoniommaina@gmail.com>
2020-04-20 05:12:15 +0300

    Initial commit

---
+++ src/main/java/Caesar.java
@@ -0,0 +1,15 @@
+public class Caesar  {
+
+    public static String encrypt(String plainText, int shift){
+        if(shift>26){
+            shift = shift%26;
+        }
+        else if(shift<0){
+            shift =(shift%26);
+        }
+    }
+
+    public static void main(String[] args){
+
+    }
+}
